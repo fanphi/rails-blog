3e9312a3c7d878f7e3dfaac5929892328100493f
fanphi <fannyp90@hotmail.com>
2022-10-07 16:24:16 +0200

    added delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,12 +59,14 @@ class ArticlesController < ApplicationController
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
+    redirect_to articles_path
 
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
