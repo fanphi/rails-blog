e3e36a31e23301b010cab8e0f1f3b0769e6fe4f6
fanphi <fannyp90@hotmail.com>
2022-10-07 12:03:39 +0200

    added articles listing view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   # GET /articles/new
   def new
     @article = Article.new
