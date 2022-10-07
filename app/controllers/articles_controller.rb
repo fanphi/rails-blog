9be342f065702d378c30ca8fce92ef191164b824
fanphi <fannyp90@hotmail.com>
2022-10-07 15:54:44 +0200

    added edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+    @article = Article.find(params[:id]) 
   end
 
   # POST /articles or /articles.json
@@ -38,15 +39,22 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    @article = Article.find(params[:id])
+   if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Article was updated successfully"
+    redirect_to @article
+   else
+    render "edit"
+   end
+    # respond_to do |format|
+    #   if @article.update(article_params)
+    #     format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+    #     format.json { render :show, status: :ok, location: @article }
+    #   else
+    #     format.html { render :edit, status: :unprocessable_entity }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /articles/1 or /articles/1.json
