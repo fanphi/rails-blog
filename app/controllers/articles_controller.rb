f6a682fe835fecde2de94b4286e8fcf6dd43f510
fanphi <fannyp90@hotmail.com>
2022-10-13 15:31:40 +0200

    added restrictions to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
   #before action läggs till tillsammans med private så att vi inte behöver upprepa methods
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:update, :edit, :destroy]
 
   # GET /articles or /articles.json
   def index
@@ -86,4 +88,11 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only edit or delete your own article"
+        redirect_to @article
+    end
+
 end
