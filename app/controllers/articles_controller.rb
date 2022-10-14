af49f13f5ae48bc4c463571f05a9632378660476
fanphi <fannyp90@hotmail.com>
2022-10-14 11:15:57 +0200

    fixed styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,8 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.all
+    # @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /articles/new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
     before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show  
-        @articles = @user.articles
+        # @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def index
-        @users = User.all
+        # @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
